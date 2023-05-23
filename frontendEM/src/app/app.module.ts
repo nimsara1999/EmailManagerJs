ce5caa907f66a511924f051cbc13f1cae07842b3
nimsaraPARAMULLA <paramullapdnt.20@uom.lk>
2023-05-23 23:22:00 +0530

    Signup dialogbox  just implemet

--- frontendEM/src/app/app.module.ts
+++ frontendEM/src/app/app.module.ts
@@ -5,16 +5,24 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
+import {HttpClientModule} from '@angular/common/http';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { SignupComponent } from './signup/signup.component';
+import { MatDialog, MatDialogModule } from '@angular/material/dialog';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    DashboardComponent
+    DashboardComponent,
+    SignupComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    BrowserAnimationsModule,
+    MatDialogModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- frontendEM/src/app/header/header.component.ts
+++ frontendEM/src/app/header/header.component.ts
@@ -1,10 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import {MatDialog, MatDialogConfig} from '@angular/material/dialog'; 
+import { SignupComponent } from '../signup/signup.component';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.css']
 })
-export class HeaderComponent {
+export class HeaderComponent implements OnInit {
+
+  constructor(private dialog:MatDialog){}
+ngOnInit(): void {
+  throw new Error('Method not implemented.');
+}
+  
+signupAction() {
+  const dialogConfig = new MatDialogConfig();
+  dialogConfig.width="550px";
+  dialogConfig.height="150px";
+  this.dialog.open(SignupComponent,dialogConfig);
+}
 
 }
